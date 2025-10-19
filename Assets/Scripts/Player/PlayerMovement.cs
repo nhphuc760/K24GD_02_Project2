0af50c40e1f986069d4d6ffd0dd963f6b87462e8
TONY <thanhtd.124010124035@vtc.edu.vn>
2025-10-19 11:18:11 +0700

    Finishing sorting layer for object and player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,29 +1,72 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement:MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float moveSpeed;
+    [SerializeField] float swimSpeed;
+    public Transform CheckGroundPoint;
+    private Tilemap[] _groundTilemaps;
     bool isMoving;
     float horizontalMovement;
     float verticalMovement;
-
+    private bool _isGrounded;
+    public bool IsGrounded
+    {
+        get => _isGrounded;
+        private set
+        {
+            _isGrounded = value;
+            //Upgrade later: Change animation state if have swimming or flying
+        }
+    }
     public bool IsMoving { get => isMoving; set => isMoving = value; }
     public float HorizontalMovement { get => horizontalMovement; }
     public float VerticalMovement { get => verticalMovement; }
+    private void Awake()
+    {
+        Tilemap[] allTilemaps = FindObjectsByType<Tilemap>(FindObjectsSortMode.None);
+        _groundTilemaps = System.Array.FindAll(allTilemaps, tm => tm.gameObject.layer == LayerMask.NameToLayer("Ground"));
+    }
 
     private void Update()
     {
-        Vector2 direct = GameInput.Ins.GetInputMovementNormalize();
         
-       
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direct = GameInput.Ins.GetInputMovementNormalize();
+
+
         isMoving = direct != Vector2.zero;
         if (isMoving)
         {
             horizontalMovement = direct.x;
             verticalMovement = direct.y;
         }
-        rb.linearVelocity = direct * moveSpeed;
-
+        IsGrounded = CheckOnGround(direct);
+        if (IsGrounded)
+        {
+            rb.linearVelocity = direct * moveSpeed;
+        }
+        else
+        {
+            rb.linearVelocity = direct * swimSpeed;
+        }
+    }
+    private bool CheckOnGround(Vector2 direction)
+    {
+        Vector3 nextPos = CheckGroundPoint.transform.position + (Vector3)(direction * 0.1f);
+        Vector3Int cellPos;
+        foreach (Tilemap tilemap in _groundTilemaps)
+        {
+            cellPos = tilemap.WorldToCell(nextPos);
+            if (tilemap.GetTile(cellPos) != null)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
