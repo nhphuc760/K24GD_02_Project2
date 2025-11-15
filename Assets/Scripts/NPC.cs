936c50c7cd06254a0cc2b4f26d66c70ec55dc5cb
nhphuc760 <dkmtaoko@gmail.com>
2025-11-15 23:22:33 +0700

    .

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -20,7 +20,7 @@ public class NPC : MonoBehaviour, IInteractable
 
     public void Interact()
     {
-        if (dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
+        if (dialogueData == null || !isDialogueActive)
             return;
         if (isDialogueActive)
         {
@@ -39,8 +39,6 @@ public class NPC : MonoBehaviour, IInteractable
         nameText.SetText(dialogueData.npcName);
         portraitImage.sprite = dialogueData.npcPortrait;
         dialoguePanel.SetActive(true);
-        PauseController.SetPause(true);
-
         StartCoroutine(TypeLine());
     }
     void NextLine()
@@ -85,6 +83,5 @@ public class NPC : MonoBehaviour, IInteractable
         isDialogueActive = false;
         dialogueText.SetText("");
         dialoguePanel.SetActive(false);
-        PauseController.SetPause(false);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,40 +3,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
-    //public Transform CheckGroundPoint;
-    //private Tilemap[] _groundTilemaps;
     bool isMoving;
     float horizontalMovement;
     float verticalMovement;
-    //private bool _isGrounded;
-    //public bool IsGrounded
-    //{
-    //    get => _isGrounded;
-    //    private set
-    //    {
-    //        _isGrounded = value;
-    //        //Upgrade later: Change animation state if have swimming or flying
-    //    }
-    //}
     public bool IsMoving => isMoving;
     public float HorizontalMovement { get => horizontalMovement; }
     public float VerticalMovement { get => verticalMovement; }
 
     private Rigidbody2D rb;
+    Vector2 direct;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //var playerFarming = GetComponent<PlayerFarming>();
-        //if (playerFarming.enabled == false) return;
-        //Tilemap[] allTilemaps = FindObjectsByType<Tilemap>(FindObjectsSortMode.None);
-        //_groundTilemaps = System.Array.FindAll(allTilemaps, tm => tm.gameObject.layer == LayerMask.NameToLayer("Ground"));
     }
 
     void Update()
     {
-        Vector2 direct = GameEventManager.Ins.gameInput.GetInputMovementNormalize();
+         direct = GameEventManager.Ins.gameInput.GetInputMovementNormalize();
         isMoving = direct != Vector2.zero;
+      
+    }
+
+    private void FixedUpdate()
+    {
         if (isMoving)
         {
             horizontalMovement = direct.x;
@@ -45,24 +35,6 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocity = direct * moveSpeed;
     }
 
-    //private bool CheckOnGround(Vector2 direction)
-    //{
-    //    if (CheckGroundPoint == null)
-    //    {
-    //        return false;
-    //    }
-    //    Vector3 nextPos = CheckGroundPoint.transform.position + (Vector3)(direction * 0.1f);
-    //    Vector3Int cellPos;
-    //    foreach (Tilemap tilemap in _groundTilemaps)
-    //    {
-    //        cellPos = tilemap.WorldToCell(nextPos);
-    //        if (tilemap.GetTile(cellPos) != null)
-    //        {
-    //            return true;
-    //        }
-    //    }
-    //    return false;
-    //}
     public Vector2 GetDirection()
     {
         return new Vector2 (horizontalMovement, verticalMovement);
